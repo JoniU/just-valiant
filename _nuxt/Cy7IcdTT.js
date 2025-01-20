8ffc9573f8c710d4dd99f46baf12c71237805821
JoniU <joni@genero.fi>
2025-01-20 08:44:44 +0200

    Updates

--- _nuxt/Cy7IcdTT.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as r,c as t,G as n}from"./BQ4X9Uqh.js";const s={};function c(e,a){return r(),t("strong",null,[n(e.$slots,"default")])}const _=o(s,[["render",c]]);export{_ as default};
--- _nuxt/x1VwUQ6V.js
+++
@@ -1,1 +0,0 @@
-import{u as r,q as o}from"./Dig5bm3C.js";import{_ as n}from"./CzDDVPVK.js";import{f as i,h as _,j as c,i as a,o as l}from"./BQ4X9Uqh.js";const x=i({__name:"index",async setup(m){let t,e;const{data:s}=([t,e]=_(()=>r(()=>o("litter").all(),"$4gANvhaSWJ")),t=await t,e(),t);return(p,f)=>(l(),c(a(n),{litters:a(s)},null,8,["litters"]))}});export{x as default};
