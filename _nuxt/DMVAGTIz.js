dc0c2dacb6fe29b2032c83a39cc1b55ab57be1d7
JoniU <joni@genero.fi>
2025-02-04 08:51:02 +0200

    Updates

--- _nuxt/DMVAGTIz.js
+++
@@ -1,1 +0,0 @@
-import{u as r,q as o}from"./BK-QpmO8.js";import{_ as n}from"./BFKcgHbw.js";import{f as i,h as _,j as c,i as a,o as l}from"./ooHtH0Rv.js";const x=i({__name:"index",async setup(m){let t,e;const{data:s}=([t,e]=_(()=>r(()=>o("litter").all(),"$4gANvhaSWJ")),t=await t,e(),t);return(p,f)=>(l(),c(a(n),{litters:a(s)},null,8,["litters"]))}});export{x as default};
